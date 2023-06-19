578abb2aa729bad28c50dbd3242171687ae5b777
parshuram kalunkhe <parshuramkalunkhe@gmail.com>
2023-06-19 21:21:35 +0530

    Add portfolio

---
+++ portfolio/script.js
@@ -0,0 +1,19 @@
+// Toggle mobile menu
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  burger.classList.toggle('active');
+});
+
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
